855a3428d43c396b9aef362aede58675a5853191
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-10-26 02:15:15 -0300

    Update app.js
    obtendo os elementos do HTML via JavaScript

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 // app.js
+
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
