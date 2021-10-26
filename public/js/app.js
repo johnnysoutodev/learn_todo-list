1b1a0f65bb230e7578933f3385ace8d862971764
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-10-26 20:13:50 -0300

    Update app.js
    selecionando elementos e guardando nomes de classes

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,15 @@
 // app.js
 
+// Select the elements
+
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Show today dates
