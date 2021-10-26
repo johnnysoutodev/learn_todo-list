0610edce23131ad4d9f1df623c4d686ba9ab2d73
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-10-26 20:45:29 -0300

    Update app.js
    configurando a data do app

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,8 @@ const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
 // Show today dates
+const options = { weekday: "long", month: "long", day: "numeric" };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("pt-BR", options);
+console.log(dateElement);
