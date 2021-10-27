6e5f8114a23c29d5d8885bed166c7dd86b6e06c6
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-10-26 21:05:28 -0300

    Update app.js
    função ToDo concluida

--- public/js/app.js
+++ public/js/app.js
@@ -23,7 +23,17 @@ dateElement.innerHTML = today.toLocaleDateString("pt-BR", options);
 function addToDo(toDo) {
 
     const item = `
-                
+                <li class="item">
+                    <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                    <p class="text">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                </li>
                 `;
 
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+
 }
+
+addToDo("Drink Coffee");
